ccbcc6a6f40e7a34f95f76bd279d2bada944fd09
David Ralph <ohlookitsderpy@protonmail.com>
2021-07-06 11:47:15 +0100

    feat: add ratelimit

--- api/item.js
+++ api/item.js
@@ -1,4 +1,14 @@
-module.exports = (req, res) => {
+const rateLimit = require('lambda-rate-limiter')({
+    interval: 60 * 1000
+}).check;
+
+module.exports = async (req, res) => {
+    try {
+        await rateLimit(50, req.headers['x-real-ip']);
+    } catch (error) {
+        return res.status(429).send({ message: 'Too many requests' });
+    }
+
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     return res.status(200).send({
